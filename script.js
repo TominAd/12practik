f4dc90f3f4ff1c4dc22a1e0cd7c941b99d21396b
TominAd <denis.goroshko1990@gmail.com>
2021-10-18 17:42:20 +0300

    1lesson

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const number0fFilms = +prompt("Сколько фильмоф вы уже простотрели", "");
 
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt("один из последних просмотренных фильмоф?", ""),
+        b = prompt("на сколько оцените его?", ""),
+        c = prompt("один из последних просмотренных фильмоф?", ""),
+        d = prompt("на сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
